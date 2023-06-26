271ae0bfa40a7c3e39bdc84ce6f14bf34fd047a0
0rphee <0rph3e@proton.me>
2023-06-25 20:30:08 -0600

    initial commit: restart project

---
+++ app/CmdlineOptions.hs
@@ -0,0 +1,37 @@
+module CmdlineOptions
+  ( options
+  , Options (..)
+  , execParser
+  )
+where
+
+import Data.ByteString.Char8 as B
+import Options.Applicative
+
+newtype Options = Options
+  { sourceCodeFile :: Maybe ByteString
+  }
+
+options :: ParserInfo Options
+options =
+  info
+    (opts <**> helper)
+    ( fullDesc
+        <> header "xolsh - a lox interpreter written in haskell "
+        <> footer
+          "still a work in progress, source code here: https://codeberg.org/0rphee/xolsh, \
+          \following the Crafting Interpreters book: https://craftinginterpreters.com/"
+        <> failureCode 64
+    )
+
+opts :: Parser Options
+opts =
+  Options
+    <$> optional
+      ( strArgument
+          ( metavar "FILENAME"
+              <> help "Input file"
+              <> action "directory"
+              <> action "file"
+          )
+      )
