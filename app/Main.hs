50515eb943b0183e7d1259fdcd4187942c97420f
0rphee <0rph3e@proton.me>
2023-07-09 13:35:12 -0600

    add outer functions for executing code

--- app/Main.hs
+++ app/Main.hs
@@ -2,14 +2,22 @@ module Main (main) where
 
 import CmdlineOptions
 import Data.ByteString.Char8 as B
-import System.Exit
 
 main :: IO ()
 main = do
   (Options sourceCodeFilepath) <- execParser options
-  case sourceCodeFilepath of
-    Just path -> do
-      B.putStrLn $ "Run file: " <> path
-    Nothing -> do
-      B.putStrLn "Start repl"
-      exitWith (ExitFailure 64)
+  maybe runPrompt runFile sourceCodeFilepath
+
+runFile :: ByteString -> IO ()
+runFile path = do
+  B.putStrLn $ "Run file: " <> path
+  fileContents <- B.readFile $ B.unpack path
+  run fileContents
+
+run :: ByteString -> IO ()
+run b = do
+  B.putStrLn b
+  undefined
+
+runPrompt :: IO ()
+runPrompt = B.putStrLn "Run prompt"
